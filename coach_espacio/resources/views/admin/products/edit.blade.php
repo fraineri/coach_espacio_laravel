61cea792e97d4b89efef46d56bb854ccbc2e753a
dianacrego <dianacrego@gmail.com>
2017-07-15 18:29:13 -0300

    cambios en el admin

---
+++ coach_espacio/resources/views/admin/products/edit.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Coach Espacios Editar</title>
+</head>
+<body>
+
+</body>
+</html>
--- coach_espacio/resources/views/admin/products/index.blade.php
+++ coach_espacio/resources/views/admin/products/index.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Indice Administrador</title>
+	<title>Coach Espacios Admin</title>
 </head>
 <body>
 <h1>Productos</h1>

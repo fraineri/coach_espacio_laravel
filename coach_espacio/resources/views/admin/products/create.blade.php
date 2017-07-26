115ab1b9465042b86a59a08aeac95695d7d53e82
dianacrego <dianacrego@gmail.com>
2017-07-26 10:20:11 -0300

    css al admin

--- coach_espacio/resources/views/admin/products/create.blade.php
+++ coach_espacio/resources/views/admin/products/create.blade.php
@@ -4,58 +4,69 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" type="text/css" href="/css/main.css">
-	<link rel="stylesheet" type="text/css" href="/css/producto.css">
-	<link href="https://fonts.googleapis.com/css?family=Yantramanav" rel="stylesheet"> 
-	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
-	<link rel="stylesheet" href="/css/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 
 	<title>C.E. | Agregar Producto</title>
 </head>
 <body>
 	<h1>Agregar Nuevo Producto o Video</h1>
+	<div class="container">
 	<form class="form-horizontal" method="POST" action="/admin/products/create" enctype="multipart/form-data">
 				{{csrf_field()}}
-				<label>Nombre:</label>
-				<input type="text" name="name" class="form-control" placeholder="Nombre del producto o video" required>
-				@if ($errors->has('name'))
-					<span class="lbl-error">
-						<strong> {{ $errors->first('name') }} </strong>
-					</span>
-				@endif
 				<br>
-				<br>
-				<label>Descripción:</label>
-				<input type="text" name="description" class="form-control" placeholder="Descripción" required>
-				@if ($errors->has('description'))
-					<span class="lbl-error">
-						<strong> {{ $errors->first('description') }} </strong>
-					</span>
-				@endif
-				<br>
-				<br>
-				<label>Precio:</label>
-				<input type="text" name="price" class="form-control" placeholder="Precio" required>
-				@if ($errors->has('price'))
+
+				<div class="form-group row">
+  					<label for="name-text-input" class="col-2 col-form-label">Nombre</label>
+  					<div class="col-10">
+						<input type="text" name="name" class="form-control" placeholder="Nombre del producto o video" id="name-text-input" required>
+						@if ($errors->has('name'))
+							<span class="lbl-error">
+							<strong> {{ $errors->first('name') }} </strong>
+							</span>
+						@endif
+					</div>
+  				</div>
+
+				<div class="form-group row">
+  					<label for="description-text-input" class="col-2 col-form-label">Descripción</label>
+  					<div class="col-10">
+						<input type="text" name="description" class="form-control" placeholder="Descripción" id="description-text-input" required>
+						@if ($errors->has('description'))
+							<span class="lbl-error">
+								<strong> {{ $errors->first('description') }} </strong>
+							</span>
+						@endif
+					</div>
+  				</div>
+
+				<div class="form-group row">
+  					<label for="price-text-input" class="col-2 col-form-label">Precio</label>
+  					<div class="col-10">
+						<input type="text" name="price" class="form-control" placeholder="Precio del producto o video" id="price-text-input" required>
+						@if ($errors->has('price'))
 					<span class="lbl-error">
 						<strong> {{ $errors->first('price') }} </strong>
 					</span>
 				@endif
-				<br>
-				<br>
-				<label>Stock:</label>
-				<input type="text" name="stock" class="form-control" placeholder="cantidad en stock" required>
-				@if ($errors->has('stock'))
-					<span class="lbl-error">
-						<strong> {{ $errors->first('stock') }} </strong>
-					</span>
-				@endif
-				<br>
-				<br>
+					</div>
+  				</div>
+				
+				<div class="form-group row">
+  					<label for="stock-number-input" class="col-2 col-form-label">Stock</label>
+  					<div class="col-10">
+						<input type="number" name="stock" class="form-control" placeholder="cantidad en stock" id="stock-number-input" required>
+						@if ($errors->has('stock'))
+							<span class="lbl-error">
+								<strong> {{ $errors->first('stock') }} </strong>
+							</span>
+						@endif
+					</div>
+  				</div>
+	
 				<!--true o false para purchable-->
 				<label>Producto listo para vender?  </label>
-				<select name="purchable" required>
+				<select class="custom-select" name="purchable" required>
+					<option selected>Seleccionar</option>
 					<option value=1>Si, Vender</option>
 					<option value=0>NO vender</option>
 				</select>
@@ -63,7 +74,8 @@
 				<br>
 				<!--categorias de la tabla categories-->
 				<label>Categoria del producto: </label>
-				<select name="category_id" required>
+				<select class="custom-select" name="category_id" required>
+					<option selected>Seleccionar</option>
 					@foreach($categories as $cat)
 						<option value="{{$cat->id}}">{{$cat->name}}</option>
 					@endforeach
@@ -71,28 +83,31 @@
 				<br>
 				<br>
 				<label>Tipo de producto:  </label>
-				<select name="type" required>
+				<select class="custom-select" name="type" required>
+					<option selected>Seleccionar</option>
 					<option value="products">Articulo</option>
 					<option value="course">Video Curso</option>
 				</select>
 				<br>
-				<br>
+
 				<!--imagen-->
 				<label class="admin-product-image">Foto de Producto</label>
 				<input type="file" name="picture" class="form-control" required>
 				<br>
-				<br>
-				<hr>
-				<!--boton para salir sin editar el producto-->
-				<div class="form-group">
-				<button type="button" class="btn btn-primary"><a href="/admin/products/">Volver a productos</a></button>	
-				</div>
-				<br>
-				<!--boton para grabar cambios-->
-				<div class="form-group">
-				<button type="submit" class="btn btn-primary">Guardar Nuevo Producto</button>	
-				</div>
 				
-		</form>
+				<div class="form-inline">
+				<!--boton para salir sin editar el producto-->
+					<a class="btn btn-primary" href="/admin/products/" role="button">Volver a productos</a>
+				<!--boton para grabar cambios-->
+					<button type="submit" class="btn btn-success">Guardar Nuevo Producto</button>	
+				</div>
+			
+
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+	</form>
+	</div>
 </body>
 </html>

adf8ef313d608879ac8b227be9c612749ce6b375
dianacrego <dianacrego@gmail.com>
2017-07-25 23:14:45 -0300

    css para admin

--- coach_espacio/resources/views/admin/categories/index-cat.blade.php
+++ coach_espacio/resources/views/admin/categories/index-cat.blade.php
@@ -4,32 +4,42 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" type="text/css" href="/css/main.css">
-	<link rel="stylesheet" type="text/css" href="/css/producto.css">
-	<link href="https://fonts.googleapis.com/css?family=Yantramanav" rel="stylesheet"> 
-	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
-	<link rel="stylesheet" href="/css/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 
 	<title>C.E. | Categorias</title>
 </head>
 <body>
-<h1>Categorías:</h1>
-	<br>
-	<div class="form-group">
-		<button type="button" class="btn btn-primary"><a href="/admin/categories/create">Nueva Categoría</a></button>	
+
+	<div class="container">
+  		<nav class="navbar navbar-light navbar-toggleable-sm" style="background-color: #e3f2fd;">
+    		<a class="navbar-brand" href="/admin/products/">Productos</a>
+    		<a class="navbar-brand" href="/admin/categories/">Categorias</a>
+  		</nav>
 	</div>
+	<br>
+<h1>Categorías:</h1>
 	<br>
 	<br>
 	<div class="">
-	<ul>
-		@foreach($categories as $cat)
-		<li>
-		<a href="/admin/categories/{{$cat->id}}/update">{{$cat->name}}</a>
-		</li>
-		@endforeach
-	</ul>
+		<ul class="list-group">
+			@foreach($categories as $cat)
+				<li class="list-group-item">
+				<a href="/admin/categories/{{$cat->id}}/update">{{$cat->name}}</a>
+				</li>
+			@endforeach
+		</ul>
 	</div>
+
+	<br>
+	<div class="form-group">
+		<a class="btn btn-primary" href="/admin/categories/create">Nueva categoría</a>		
+	</div>
+
 	{{$categories->links()}}
+
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
 </body>
 </html>
--- coach_espacio/resources/views/admin/products/index.blade.php
+++ coach_espacio/resources/views/admin/products/index.blade.php
@@ -4,34 +4,45 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" type="text/css" href="/css/main.css">
-	<link rel="stylesheet" type="text/css" href="/css/producto.css">
-	<link href="https://fonts.googleapis.com/css?family=Yantramanav" rel="stylesheet"> 
-	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
-	<link rel="stylesheet" href="/css/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
 
 	<title>C.E. | Administrador</title>
 </head>
 <body>
+
+	<div class="container">
+  		<nav class="navbar navbar-light navbar-toggleable-sm" style="background-color: #e3f2fd;">
+    		<a class="navbar-brand" href="/admin/products/">Productos</a>
+    		<a class="navbar-brand" href="/admin/categories/">Categorias</a>
+  		</nav>
+	</div>
+	<br>
+
 	<h1>Productos y Videos</h1>
 	<br>
-	<div class="form-group">
-		<button type="button" class="btn btn-primary"><a href="/admin/products/create">Nuevo Producto</a></button>	
-	</div>
+	
 	<br>
 	<br>
 	<div class="">
-	<ul>
-		@foreach($products as $prod)
-		<li>
-		<a href="/admin/products/{{$prod->id}}/update">{{$prod->name}}</a>
-		</li>
-		@endforeach
-	</ul>
+		<ul class="list-group">
+			@foreach($products as $prod)
+				<li class="list-group-item">
+				<a href="/admin/products/{{$prod->id}}/update">{{$prod->name}}</a>
+				</li>
+			@endforeach
+		</ul>
 	</div>
+	<br>
+	<div class="form-group">
+		<a class="btn btn-primary" href="/admin/products/create">Nuevo Producto</a>	
+	</div>
+
 	{{$products->links()}}
 
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 </body>
 </html>
 

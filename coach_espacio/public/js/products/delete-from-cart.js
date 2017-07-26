b570161e4ed884bab79de9c1a5d3a2c070b7c8cd
Lucia <luuciarra@gmail.com>
2017-07-26 00:25:28 -0300

    ajax 2

--- coach_espacio/public/js/products/delete-from-cart.js
+++ coach_espacio/public/js/products/delete-from-cart.js
@@ -1,20 +1,23 @@
-forms =  Array.from(document.getElementById("items-cont").children).forEach(function(i){
-	i.querySelector(".item-delete").addEventListener("click",function(e){
-		id = i.querySelector("[name=id]").value;
-		var req = new XMLHttpRequest();
-		req.onreadystatechange = function () {
-			if (this.readyState === 4) {
-	    		if (this.status === 200) {
-	    			if(this.responseText == "ok"){
-	    				location.reload();
-	    			}
-	    		}
-	    	}
-	    }
-	    req.open('POST', '/shop/delete');
-	    var data = new FormData();
-	    data.append("id",id);
-	    data.append("_token", "{{ csrf_token() }}");
-		req.send(data);
-	})
-});
+if (document.querySelector(".item-summary")) {
+	forms =  Array.from(document.getElementById("items-cont").children).forEach(function(i){
+		i.querySelector(".item-delete").addEventListener("click",function(e){
+			id = i.querySelector("[name=id]").value;
+			var req = new XMLHttpRequest();
+			req.onreadystatechange = function () {
+				if (this.readyState === 4) {
+		    		if (this.status === 200) {
+		    			if(this.responseText == "ok"){
+		    				location.reload();
+		    			}
+		    		}
+		    	}
+		    }
+		    req.open('POST', '/shop/delete');
+		    token = i.querySelector("[name = _token]").value;
+		    var data = new FormData();
+		    data.append("id",id);
+		    data.append("_token", token);
+			req.send(data);
+		})
+	});
+}
--- coach_espacio/resources/views/shop/shop.blade.php
+++ coach_espacio/resources/views/shop/shop.blade.php
@@ -68,26 +68,5 @@
 
 	</div>
 
-	<script>
-		forms =  Array.from(document.getElementById("items-cont").children).forEach(function(i){
-			i.querySelector(".item-delete").addEventListener("click",function(e){
-				id = i.querySelector("[name=id]").value;
-				var req = new XMLHttpRequest();
-				req.onreadystatechange = function () {
-					if (this.readyState === 4) {
-			    		if (this.status === 200) {
-			    			if(this.responseText == "ok"){
-			    				location.reload();
-			    			}
-			    		}
-			    	}
-			    }
-			    req.open('POST', '/shop/delete');
-			    var data = new FormData();
-			    data.append("id",id);
-			    data.append("_token", "{{ csrf_token() }}");
-				req.send(data);
-			})
-		});
-	</script>
+	<script src = "/js/products/delete-from-cart.js"></script>
 @endsection

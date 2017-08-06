33d7d189bd2cd38fb91b955baf1e6ac48d21db07
dianacrego <dianacrego@gmail.com>
2017-08-06 17:47:22 -0300

    agregue columna delivered a tabla sales

--- coach_espacio/app/Sale.php
+++ coach_espacio/app/Sale.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sale extends Model{
     protected $fillable = [
-        'user_id', 'total','status','name','surname', 'address','city','province','cp','phone','card_name','card_number','card_code','card_expire'
+        'user_id', 'total','status','name','surname', 'address','city','province','cp','phone','card_name','card_number','card_code','card_expire', 'delivered'
     ];
 
 
